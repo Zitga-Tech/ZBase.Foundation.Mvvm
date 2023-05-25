84671e6ead5e3dd8220c91a51894f32e254dea18
Grashaar <grashaar@outlook.com>
2023-05-25 23:53:47 +0700

    Observable collections should not use Union

--- Packages/ZBase.Foundation.Mvvm/Mvvm.ObservableCollections/Common/CollectionChangeEventArgs.cs
+++ Packages/ZBase.Foundation.Mvvm/Mvvm.ObservableCollections/Common/CollectionChangeEventArgs.cs
@@ -1,143 +1,99 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
-using ZBase.Foundation.Mvvm.Unions;
 
 namespace ZBase.Foundation.Mvvm.ObservableCollections
 {
     public readonly struct CollectionChangeEventArgs<T>
     {
-        public readonly IObservableCollection<T> Sender;
         public readonly CollectionAction Action;
-        public readonly Union Value;
+        public readonly int Index;
+        public readonly int Count;
+        public readonly T Item;
+        public readonly IEnumerable<T> Items;
+        public readonly IObservableCollection<T> Sender;
 
-        public CollectionChangeEventArgs(IObservableCollection<T> sender, CollectionAction action, in Union value)
+        public CollectionChangeEventArgs(
+              IObservableCollection<T> sender
+            , CollectionAction action
+            , T item = default
+            , int index = 0
+            , int count = 0
+            , IEnumerable<T> items = null
+        )
         {
-            Sender = sender;
             Action = action;
-            Value = value;
+            Index = index;
+            Count = count;
+            Item = item;
+            Items = items ?? Enumerable.Empty<T>();
+            Sender = sender;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static CollectionChangeEventArgs<T> Add(IObservableCollection<T> sender, T item)
         {
-            return new CollectionChangeEventArgs<T>(sender, CollectionAction.Add, item.AsUnion());
+            return new CollectionChangeEventArgs<T>(sender, CollectionAction.Add, item);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static CollectionChangeEventArgs<T> Insert(IObservableCollection<T> sender, int index, T item)
         {
-            return new CollectionChangeEventArgs<T>(
-                  sender
-                , CollectionAction.Insert
-                , new IndexedValueUnion(new IndexedValue<T>(index, item))
-            );
+            return new CollectionChangeEventArgs<T>(sender, CollectionAction.Insert, item, index);
         }
         
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static CollectionChangeEventArgs<T> InsertRange(IObservableCollection<T> sender, int index, IEnumerable<T> items)
         {
-            return new CollectionChangeEventArgs<T>(
-                  sender
-                , CollectionAction.InsertRange
-                , new IndexedEnumerableUnion(new IndexedEnumerable<T>(index, items))
-            );
+            return new CollectionChangeEventArgs<T>(sender, CollectionAction.InsertRange, index: index, items: items);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static CollectionChangeEventArgs<T> AddRange(IObservableCollection<T> sender, IEnumerable<T> items)
         {
-            return new CollectionChangeEventArgs<T>(
-                  sender
-                , CollectionAction.Insert
-                , new RefTypeHandleUnion(new Enumerable<T>(items))
-            );
+            return new CollectionChangeEventArgs<T>(sender, CollectionAction.AddRange, items: items);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static CollectionChangeEventArgs<T> Remove(IObservableCollection<T> sender, T item)
         {
-            return new CollectionChangeEventArgs<T>(sender, CollectionAction.Remove, item.AsUnion());
+            return new CollectionChangeEventArgs<T>(sender, CollectionAction.Remove, item);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static CollectionChangeEventArgs<T> RemoveRange(IObservableCollection<T> sender, IEnumerable<T> items)
         {
-            return new CollectionChangeEventArgs<T>(
-                  sender
-                , CollectionAction.RemoveRange
-                , new RefTypeHandleUnion(new Enumerable<T>(items))
-            );
+            return new CollectionChangeEventArgs<T>(sender, CollectionAction.RemoveRange, items: items);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static CollectionChangeEventArgs<T> RemoveAt(IObservableCollection<T> sender, int index)
         {
-            return new CollectionChangeEventArgs<T>(sender, CollectionAction.RemoveAt, index);
+            return new CollectionChangeEventArgs<T>(sender, CollectionAction.RemoveAt, index: index);
         }
         
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static CollectionChangeEventArgs<T> RemoveRangeAt(IObservableCollection<T> sender, int index, int count)
         {
-            return new CollectionChangeEventArgs<T>(
-                  sender
-                , CollectionAction.RemoveRangeAt
-                , new CollectionRange(index, count).AsUnion()
-            );
+            return new CollectionChangeEventArgs<T>(sender, CollectionAction.RemoveAt, index: index, count: count);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static CollectionChangeEventArgs<T> Replace(IObservableCollection<T> sender, T item)
+        {
+            return new CollectionChangeEventArgs<T>(sender, CollectionAction.Replace, item);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static CollectionChangeEventArgs<T> ReplaceAt(IObservableCollection<T> sender, int index, T item)
         {
-            return new CollectionChangeEventArgs<T>(
-                  sender
-                , CollectionAction.ReplaceAt
-                , new IndexedValueUnion(new IndexedValue<T>(index, item))
-            );
+            return new CollectionChangeEventArgs<T>(sender, CollectionAction.ReplaceAt, item, index: index);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static CollectionChangeEventArgs<T> Clear(IObservableCollection<T> sender)
         {
-            return new CollectionChangeEventArgs<T>(sender, CollectionAction.Clear, Union.Undefined);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static CollectionChangeEventArgs<KeyValuePair<TKey, TValue>> Add<TKey, TValue>(
-              IObservableCollection<KeyValuePair<TKey, TValue>> sender
-            , in KeyValuePair<TKey, TValue> item
-        )
-        {
-            return new CollectionChangeEventArgs<KeyValuePair<TKey, TValue>>(
-                  sender
-                , CollectionAction.Add
-                , new KeyedValueUnion(new KeyedValue<TKey, TValue>(item))
-            );
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static CollectionChangeEventArgs<KeyValuePair<TKey, TValue>> Remove<TKey, TValue>(
-              IObservableCollection<KeyValuePair<TKey, TValue>> sender
-            , in KeyValuePair<TKey, TValue> item
-        )
-        {
-            return new CollectionChangeEventArgs<KeyValuePair<TKey, TValue>>(
-                  sender
-                , CollectionAction.Remove
-                , new KeyedValueUnion(new KeyedValue<TKey, TValue>(item))
-            );
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static CollectionChangeEventArgs<KeyValuePair<TKey, TValue>> Replace<TKey, TValue>(
-              IObservableCollection<KeyValuePair<TKey, TValue>> sender
-            , in KeyValuePair<TKey, TValue> item
-        )
-        {
-            return new CollectionChangeEventArgs<KeyValuePair<TKey, TValue>>(
-                  sender
-                , CollectionAction.Replace
-                , new KeyedValueUnion(new KeyedValue<TKey, TValue>(item))
-            );
+            return new CollectionChangeEventArgs<T>(sender, CollectionAction.Clear);
         }
     }
 }
